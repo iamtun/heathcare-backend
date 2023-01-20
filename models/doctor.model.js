d65a1dd81f447e0af8c284f530674113c95923b6
iamtun <letuan19431791.iuh@gmail.com>
2023-01-20 17:30:07 +0700

    upload image when create doctor & patient info

--- models/doctor.model.js
+++ models/doctor.model.js
@@ -1,21 +1,24 @@
 import mongoose, { Schema } from 'mongoose';
 
-const doctorSchema = new Schema({
-    person: {
-        type: Schema.Types.ObjectId,
-        ref: 'persons',
+const doctorSchema = new Schema(
+    {
+        person: {
+            type: Schema.Types.ObjectId,
+            ref: 'persons',
+        },
+        isAccepted: {
+            type: Boolean,
+            default: false,
+        },
+        rating: {
+            type: Number,
+            min: 0,
+            max: 5,
+            default: 5,
+        },
     },
-    isAccepted: {
-        type: Boolean,
-        default: false,
-    },
-    rating: {
-        type: Number,
-        min: 0,
-        max: 5,
-        default: 5,
-    },
-});
+    { timestamps: true }
+);
 
 const Doctor = mongoose.model('doctors', doctorSchema);
 export default Doctor;
