b279282bd2122c2fe58d7a43a1ba7f5913e233be
iamtun <letuan19431791.iuh@gmail.com>
2023-01-30 21:01:17 +0700

    change method post to put when update

--- controllers/doctor.controller.js
+++ controllers/doctor.controller.js
@@ -70,7 +70,7 @@ const updateDoctorInfoById = async (req, res, next) => {
 
                     res.status(201).json({
                         status: 'success',
-                        data: personModelUpdated,
+                        data: oldPerson,
                     });
                 } else {
                     return next(
