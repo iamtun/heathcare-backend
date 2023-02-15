40894e3a556d1ea68fca324fd83dd8697bcce024
iamtun <letuan19431791.iuh@gmail.com>
2023-02-15 21:33:39 +0700

    get all - find by id in api rule

--- controllers/admin/rule.controller.js
+++ controllers/admin/rule.controller.js
@@ -31,4 +31,29 @@ const createRule = async (req, res, next) => {
     }
 };
 
-export default { createRule };
+const updateRule = async (req, res, next) => {
+    const { rule } = req;
+
+    if (rule === RULE_ADMIN) {
+        const { start, end, notification, type } = req.body;
+        if (start && end && notification && type) {
+            return Base.updateOne(Rule)(req, res, next);
+        } else {
+            res.status(401).json({
+                status: STATUS_FAIL,
+                error: MESSAGE_NO_ENOUGH_IN_4,
+            });
+        }
+    } else {
+        return next(
+            new AppError(403, STATUS_FAIL, MESSAGE_NO_PERMISSION),
+            req,
+            res,
+            next
+        );
+    }
+};
+const getAllRules = Base.getAll(Rule);
+const findRuleById = Base.getOne(Rule);
+
+export default { createRule, getAllRules, findRuleById, updateRule };
--- models/rule.model.js
+++ models/rule.model.js
@@ -1,26 +1,29 @@
 import mongoose, { Schema } from 'mongoose';
 
-const ruleSchema = new Schema({
-    start: {
-        type: Number,
-        min: 1,
+const ruleSchema = new Schema(
+    {
+        start: {
+            type: Number,
+            min: 1,
+        },
+        end: {
+            type: Number,
+            min: 1,
+        },
+        notification: {
+            type: String,
+            require: [true, 'Vui lòng nhập thông báo để hiển thị'],
+        },
+        type: {
+            type: String,
+            require: [
+                true,
+                'Vui lòng nhập kiểu để có thể phân biệt, VD: BMI | GLYCEMIC',
+            ],
+        },
     },
-    end: {
-        type: Number,
-        min: 1,
-    },
-    notification: {
-        type: String,
-        require: [true, 'Vui lòng nhập thông báo để hiển thị'],
-    },
-    type: {
-        type: String,
-        require: [
-            true,
-            'Vui lòng nhập kiểu để có thể phân biệt, VD: BMI | GLYCEMIC',
-        ],
-    },
-});
+    { timestamps: true }
+);
 
 const Rule = mongoose.model('rules', ruleSchema);
 export default Rule;
