74090abc1769e3d008dc26dc8b6bd77c7c32d90f
iamtun <letuan19431791.iuh@gmail.com>
2023-02-23 21:10:25 +0700

    handle add app-error

--- app.js
+++ app.js
@@ -23,8 +23,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api/v1/auth', AuthRouters);
